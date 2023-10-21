dd318b1d081dccc57a92867ef5aee21cd55da6c7
a.taranenko <i_okatv_i@mail.ru>
2023-10-21 23:48:10 +0300

    feat: Доработаны ракетка и шарик, добавлено удаление шариков

--- Assets/entities/Ball/Ball.cs
+++ Assets/entities/Ball/Ball.cs
@@ -1,7 +1,10 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Ball : AbstractBall {
   private new Rigidbody2D rigidbody;
+  private List<Action> destroyCallbacks = new();
 
   public override void Launch(Vector2 force) {
     if (rigidbody.bodyType == RigidbodyType2D.Dynamic) return;
@@ -20,5 +23,18 @@ public class Ball : AbstractBall {
     if (!wall) return;
 
     wall.DealDamage();
+  }
+
+  override public Action SubscribeDestroy(Action callback) {
+    destroyCallbacks.Add(callback);
+
+    return () => destroyCallbacks.Remove(callback);
+  }
+  override public void UnsubscribeDestroy(Action callback) {
+    destroyCallbacks.Remove(callback);
+  }
+
+  private void OnDestroy() {
+    destroyCallbacks.ForEach(item => item());
   }
 }
