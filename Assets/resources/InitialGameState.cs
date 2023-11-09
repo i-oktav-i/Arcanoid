1e0d14229536667e14459e313dd44e8479fdb016
Elizaveta Zalomnova <61969421+handlessdude@users.noreply.github.com>
2023-11-10 02:55:12 +0300

    no-pull: game state reset

---
+++ Assets/resources/InitialGameState.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+static class InitialGameState {
+
+  public static readonly int pointsPerBlockDestruction = 1;
+  public static readonly int level = 1;
+  public static readonly int ballsCnt = 6;
+  public static readonly int points = 0;
+
+}
--- Assets/shared/Scripts/GameDataScript.cs
+++ Assets/shared/Scripts/GameDataScript.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "GameData", menuName = "Game Data", order = 51)]
 public class GameDataScript : ScriptableObject {
-  public int level = 1;
-  public int balls = 6;
-  public int points = 0;
 
-  public readonly int pointsPerBlockDestruction = 1;
+  public int level = InitialGameState.level;
+  public int balls = InitialGameState.ballsCnt;
+  public int points = InitialGameState.points;
+
+  public bool resetOnStart;
+
+  public void Reset()
+  {
+    level = InitialGameState.level;
+    balls = InitialGameState.ballsCnt;
+    points = InitialGameState.points;
+  }
 }
